295f5beacae6b7d3fabdf8eb8ce50aaa3a59898c
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-07-06 14:12:55 -0300

    🔨 server listen to forwarded port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const TUNNEL_FORWARD_PORT = configService.get<number>('TUNNEL_FORWARD_PORT');
   await app.listen(3000);
 }
 bootstrap();
