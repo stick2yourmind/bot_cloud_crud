fc732e0d3fbcfd95621662ce3686908e933e9921
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-07-07 04:35:57 -0300

    🔨 crud method for messages at firebase service

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException, NotFoundException } from '@nestjs/common';
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 import { cert } from 'firebase-admin/app';
 import { serviceAccount } from 'src/config/firebase.config';
+import { CreateMessageDto, EditMessageDto } from 'src/message/dto';
 
 @Injectable()
 export class FirebaseService {
@@ -16,6 +17,60 @@ export class FirebaseService {
 
       this.firestore = getFirestore();
       this.messageCollection = this.firestore.collection('messages');
+    }
+  }
+
+  async createMessage(dto: CreateMessageDto) {
+    try {
+      const res = await this.messageCollection.add(dto);
+
+      return { id: res.id, ...dto };
+    } catch (error) {
+      throw new ServiceUnavailableException();
+    }
+  }
+
+  async getAllMessages() {
+    try {
+      const snapshot = await this.messageCollection.get();
+      const messages = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      return messages;
+    } catch (error) {
+      throw new ServiceUnavailableException();
+    }
+  }
+
+  async getMessage(id: string) {
+    try {
+      const doc = await this.messageCollection.doc(id).get();
+      if (!doc.exists) throw new NotFoundException();
+      return { id, ...doc.data() };
+    } catch (error) {
+      if (error instanceof NotFoundException) throw new NotFoundException();
+      throw new ServiceUnavailableException();
+    }
+  }
+
+  async deleteMessage(id: string) {
+    try {
+      await this.messageCollection.doc(id).delete({ exists: true });
+      return { id };
+    } catch (error) {
+      if (error?.code === 5) throw new NotFoundException();
+      throw new ServiceUnavailableException();
+    }
+  }
+
+  async updateMessage(id: string, dto: EditMessageDto) {
+    try {
+      await this.messageCollection.doc(id).update({ message: dto.message });
+      return { id, ...dto };
+    } catch (error) {
+      if (error?.code === 5) throw new NotFoundException();
+      throw new ServiceUnavailableException();
     }
   }
 }
