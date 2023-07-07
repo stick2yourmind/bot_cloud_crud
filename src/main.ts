b7b8b5fb406a2e5f66070b15411445c6ffd56bde
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-07-07 03:58:07 -0300

    🔨 add validation of request body

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const TUNNEL_FORWARD_PORT = configService.get<number>('TUNNEL_FORWARD_PORT');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(TUNNEL_FORWARD_PORT);
 }
 bootstrap();
