dbd3f09aa1b592e8741dd61c5d49f18f9f3e4462
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-07-07 04:02:18 -0300

    💩 bot get all messages from firebase

--- src/bot/bot.controller.ts
+++ src/bot/bot.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { Chat } from 'src/decorator/chat.decorator';
+import { FirebaseService } from 'src/firebase/firebase.service';
 import { telegramApi } from 'src/utils/telegramApi.util';
 
 @Controller('bot')
 export class BotController {
+  constructor(private firebase: FirebaseService) {}
   @Get('')
   async getMessages(@Req() req: Request, @Chat('chatId') chatId: string, @Chat('text') text: string) {
     console.log('🚀 ~ file: bot.controller.ts:9 ~ BotController ~ getMessages ~ req:', req.url);
     console.log('🚀 ~ file: bot.controller.ts:13 ~ GET');
-    await telegramApi.reply({ chatId, text: text !== '' ? text : '.' });
+
+    const snapshot = await this.firebase.messageCollection.get();
+    const messages = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    // return messages;
+    await telegramApi.reply({ chatId, text: JSON.stringify(messages) });
     console.log('OK');
     return 'OK';
   }
