9b74e82d722c47b087d0b72fc14a955f8b8c0d14
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-07-06 23:03:54 -0300

    🔇 bot middleware logs

--- src/bot/bot.middleware.ts
+++ src/bot/bot.middleware.ts
@@ -4,16 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class BotMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
     const isCommand = req.body?.message?.entities?.some((entity) => entity.type === 'bot_command');
-    console.log('🚀 ~ file: bot.middleware.ts:9 ~ BotMiddleware ~ use ~ req.body:', JSON.stringify(req.body));
-    console.log('🚀 ~ file: bot.middleware.ts:10 ~ BotMiddleware ~ use ~ isCommand:', isCommand);
     if (!isCommand) throw new HttpException('Processed', HttpStatus.OK);
     const [command, ...textArr] = req.body?.message?.text.split(' ');
     const text = textArr.join(' ');
-    console.log('🚀 ~ file: bot.middleware.ts:11 ~ BotMiddleware ~ use ~ req.method:', req.method);
-    console.log('🚀 ~ file: bot.middleware.ts:11 ~ BotMiddleware ~ use ~ command:', command);
-    console.log('🚀 ~ file: bot.middleware.ts:13 ~ BotMiddleware ~ use ~ text:', text);
     switch (command) {
       case '/get':
         req.method = 'GET';
