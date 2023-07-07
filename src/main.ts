c61ef8a83761eee2cd2c7934ba374026803722ed
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2023-07-06 22:25:04 -0300

    🐛 server listening to tunnel port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const TUNNEL_FORWARD_PORT = configService.get<number>('TUNNEL_FORWARD_PORT');
-  await app.listen(3000);
+  await app.listen(TUNNEL_FORWARD_PORT);
 }
 bootstrap();
